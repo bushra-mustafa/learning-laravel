4cebec4f42704bd869720a31c417757a10e075a6
Bushra Mustafa <81512285+bushra-mustafa@users.noreply.github.com>
2021-07-20 01:30:28 +0300

    DB quer Insert Data Part 19

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 use App\Models\Category ;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+
 use Auth;
 
 
 class CategoryController extends Controller
 {
     public function Allcategory(){
+        $
         return view('admin.category.index');
     }
     public function Addcategory(Request  $request){
@@ -22,15 +25,19 @@ class CategoryController extends Controller
         'category_name.max' => ' Category Less Then 255Char '
     ]);
 
-    Category::insert([
-        'category_name'=>$request->category_name,
-        'user_id' =>Auth::user()->id,
-        'created_at'=>Carbon::now()
-    ]);
+    // Category::insert([
+    //     'category_name'=>$request->category_name,
+    //     'user_id' =>Auth::user()->id,
+    //     'created_at'=>Carbon::now()
+    // ]);
     // $category= new Category();
     // $category->category_name= $request->category_name;
     // $category->user_id=Auth::user()->id;
     // $category->save();
+$data=array();
+$data['category_name']=$request->category_name;
+$data['user_id']=Auth::user()->id;
+DB::table('categories')->insert($data);
 
     return Redirect()->back()->with('success' , 'Category Inserted Succesfully' );
 
